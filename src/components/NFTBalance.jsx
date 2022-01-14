b85215eb1ccc93a3a926e97c49e446fd99a28763
Katharine Francis <katie-francis@hotmail.com>
2022-01-14 14:51:59 -0800

    NFTBalance

--- src/components/NFTBalance.jsx
+++ src/components/NFTBalance.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useMoralis, useNFTBalances } from "react-moralis";
 import { Card, Image, Tooltip, Modal, Input, Skeleton } from "antd";
 import { FileSearchOutlined, SendOutlined, ShoppingCartOutlined } from "@ant-design/icons";
@@ -22,14 +22,25 @@ const styles = {
 };
 
 function NFTBalance() {
-  const { data: NFTBalances } = useNFTBalances();
+  const { data: NFTBalances, getNFTBalances } = useNFTBalances();
   const { Moralis, chainId } = useMoralis();
   const [visible, setVisibility] = useState(false);
+  //const [nfts, setNFTs] = useState(null)
   const [receiverToSend, setReceiver] = useState(null);
   const [amountToSend, setAmount] = useState(null);
   const [nftToSend, setNftToSend] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const { verifyMetadata } = useVerifyMetadata();
+
+  const fetchNFTs = useCallback(async () => {
+    let response = await getNFTBalances({ params: { chain: "0x13881" } })
+    console.log("NFT response: ", response)
+    //setNFTs(response)
+  })
+
+  // useEffect(() => {
+  //   fetchNFTs()
+  // }, [NFTBalances])
 
   async function transfer(nft, amount, receiver) {
     const options = {
@@ -68,6 +79,7 @@ function NFTBalance() {
   return (
     <div style={{ padding: "15px", maxWidth: "1030px", width: "100%" }}>
       <h1>🖼 NFT Balances</h1>
+      {NFTBalances && NFTBalances.total === 0 ? <div><h1>No results</h1></div> : <div><h1>Results</h1></div>}
       <div style={styles.NFTs}>
         <Skeleton loading={!NFTBalances?.result}>
           {NFTBalances?.result &&
